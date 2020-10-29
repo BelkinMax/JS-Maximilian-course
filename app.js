f58a168198a7ca810ebd692e0a96430d6215ebd2
BelkinMax <belkinurban@gmail.com>
2020-10-29 14:01:35 +0100

    194. forEach()

---
+++ app.js
@@ -0,0 +1,18 @@
+const prices = [10.99, 6.99, 8.39, 2.19, 14.99];
+const tax = .21;
+
+const adjustedPrices = [];
+
+// for (const price of prices) {
+//   adjustedPrices.push(price * (1 + tax));
+// }
+
+prices.forEach((price, idx, prices) => {
+  const priceObj = {index: idx, adjPrice: price * (1 + tax)}
+  adjustedPrices.push(priceObj);
+})
+
+console.log(prices);
+console.log(adjustedPrices);
+
+/* MAP */
