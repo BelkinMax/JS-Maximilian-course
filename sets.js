209e2bf8c2f9d3187cbf78cbda5037bc2dd81db7
BelkinMax <belkinurban@gmail.com>
2020-10-29 16:14:15 +0100

    Assignment arrays

---
+++ sets.js
@@ -0,0 +1,40 @@
+/* SETS & MAPS */
+
+const ids = new Set([1, 2, 3]);
+ids.add(0);
+ids.add(2);
+console.log(ids);
+
+// MAPS
+
+const person1 = {name: 'Max'};
+const person2 = {name: 'Anna'};
+
+const personData = new Map([[person1, [{date: '20/10/20', price: 10}]]]);
+personData.set(person2, [{date: '13/05/20', price: '14'}]);
+
+console.log(personData);
+console.log(personData.get(person1));
+
+for(const entry of personData) {
+  console.log(entry)
+}
+
+// WEAK SET
+
+let pers = {name: 'Max'};
+const persns = new WeakSet();
+persns.add(pers);
+
+//pers = null;
+
+console.log(persns)
+
+// WEAK MAP
+
+const persData = new WeakMap();
+persData.set(pers, 'Some info');
+
+pers = null;
+
+console.log(persData)
