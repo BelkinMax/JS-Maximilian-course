04bdefcad7fabe33fb82d287c1d566d7344c094f
BelkinMax <belkinurban@gmail.com>
2020-10-29 14:07:25 +0100

    195. Map

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+/* MAP */
+
 const prices = [10.99, 6.99, 8.39, 2.19, 14.99];
 const tax = .21;
-const adjustedPrices = [];
 
-
+const taxAdjPrices = prices.map((price, idx, prices) => {
+  const priceObj = {
+    index: idx,
+    taxAdjPrice: price * (1 + tax)
+  };
+  return priceObj;
+});
 
 console.log(prices);
-console.log(adjustedPrices);
+console.log(taxAdjPrices);
