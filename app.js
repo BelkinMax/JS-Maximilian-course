ecfdbdb670d6f7f9eac1223b057fc17a722bb37c
BelkinMax <belkinurban@gmail.com>
2020-10-29 15:48:09 +0100

    206. Sets & maps methods

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 /* SETS & MAPS */
+
+const ids = new Set([1, 2, 3]);
+ids.add(0);
+ids.add(2);
+console.log(ids);
+
+// MAPS
+
+const person1 = {name: 'Max'};
+const person2 = {name: 'Anna'};
+
+const personData = new Map([[person1, [{date: '20/10/20', price: 10}]]]);
+personData.set(person2, [{date: '13/05/20', price: '14'}]);
+
+console.log(personData);
+console.log(personData.get(person1));
+
+for(const entry of personData) {
+  console.log(entry)
+}
