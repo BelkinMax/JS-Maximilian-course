6d8659248735411f4db446450fe4a96afd38d21c
BelkinMax <belkinurban@gmail.com>
2020-11-02 07:01:00 +0100

    Section 15

--- app.js
+++ app.js
@@ -8,8 +8,24 @@ console.log(add(12, 15));
 
 // Impure function
 function addRandom(num1, num2) {
-  return (num1 + num2 + (Math.random() * 10)).toFixed();
+  return (num1 + num2 + Math.random() * 10).toFixed();
 }
 
 console.log(addRandom(1, 5));
 console.log(addRandom(12, 15));
+
+// Preconfigured function
+// Closures
+
+function createTaxCalculator(tax) {
+  function calculateTax(amount) {
+    return tax * amount;
+  }
+  return calculateTax;
+}
+
+const calculateTaxAnount = createTaxCalculator(0.2);
+const calculateIncomeTax = createTaxCalculator(0.5);
+
+console.log(calculateTaxAnount(100));
+console.log(calculateIncomeTax(200));
